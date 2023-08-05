7f14f879180eac5693b817422aa363ac8b2cad59
Talwinder Singh <singhtalwinder790@gmail.com>
2023-08-05 16:15:34 +0530

    feat: add abstract world

---
+++ lib/abstract.world.ts
@@ -0,0 +1,40 @@
+import { INestApplication, NestModule, Type } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ICucumberKitConfig } from './config.interface';
+
+type Module = Type<NestModule> | any;
+
+export class AbstractWorld {
+  private appModule: Module;
+  protected config: ICucumberKitConfig;
+  public app: INestApplication | null;
+
+  constructor(appModule: Module, config: ICucumberKitConfig) {
+    this.appModule = appModule;
+    this.config = config;
+    this.app = null;
+  }
+
+  async startApp(): Promise<void> {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [this.appModule],
+    }).compile();
+
+    this.app = moduleFixture.createNestApplication();
+    await this.app.init();
+    await this.configureApp();
+  }
+
+  async stopApp(): Promise<void> {
+    if (this.app) {
+      await this.app.close();
+      this.app = null;
+    }
+  }
+
+  private async configureApp(): Promise<void> {
+    if (this.config.appConfigure) {
+      await this.config.appConfigure(this.app);
+    }
+  }
+}
