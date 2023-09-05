af9c9d3cb82dd06e483ce356896a607c6f3117fa
Talwinder Singh <singhtalwinder790@gmail.com>
2023-09-05 23:15:33 +0530

    refactor: use env for port

--- packages/mongodb/features/app/main.ts
+++ packages/mongodb/features/app/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import environment from './environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(environment.port || 3000);
 }
 bootstrap();
